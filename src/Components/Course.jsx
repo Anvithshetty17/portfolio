74cb0ff75c3d6740a48b39e6ed16c994ce91ede2
Anvith shetty <161817374+Anvithshetty17@users.noreply.github.com>
2025-08-03 16:01:28 +0530

    Update Course.jsx

--- src/Components/Course.jsx
+++ src/Components/Course.jsx
@@ -4,7 +4,7 @@ import Autoplay from "embla-carousel-autoplay";
 import c1 from "../assets/c1.png";
 
 import c2 from "../assets/c2.png";
-import c1 from "../assets/c3.png";
+import c3 from "../assets/c3.png";
 
 const Course = () => {
     const sections=[ 
